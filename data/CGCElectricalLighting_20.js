7653923048d0fbdc5b7ee5b4d1892dcc098b1707
davidbwilliamson <david.w@ihug.com.au>
2026-03-19 14:04:18 +1100

    Add files via upload

---
+++ data/CGCElectricalLighting_20.js
@@ -0,0 +1,1 @@
+var json_CGCElectricalLighting_20 = {"type":"FeatureCollection","name":"CGCElectricalLighting_20","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
