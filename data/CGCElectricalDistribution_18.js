7a154db32d1d2c8a057611634c41f09900c9a225
davidbwilliamson <david.w@ihug.com.au>
2026-03-19 11:09:04 +1100

    Add files via upload

---
+++ data/CGCElectricalDistribution_18.js
@@ -0,0 +1,1 @@
+var json_CGCElectricalDistribution_18 = {"type":"FeatureCollection","name":"CGCElectricalDistribution_18","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
