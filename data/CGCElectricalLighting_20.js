1a00a5bd81a5e3aad047e01e1d1cebb05745a6d8
davidbwilliamson <david.w@ihug.com.au>
2026-03-19 15:36:08 +1100

    Delete data directory

--- data/CGCElectricalLighting_20.js
+++
@@ -1,1 +0,0 @@
-var json_CGCElectricalLighting_20 = {"type":"FeatureCollection","name":"CGCElectricalLighting_20","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
--- data/CGCIrrigationDistribution_3.js
+++
@@ -1,1 +0,0 @@
-var json_CGCIrrigationDistribution_3 = {"type":"FeatureCollection","name":"CGCIrrigationDistribution_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
